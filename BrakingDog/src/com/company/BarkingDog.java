7d13437b40952da4f9b18fa31ce175c50832b2e6
Jun Jian Shen <jshen9085@gmail.com>
2019-03-01 21:12:52 -0500

    BarkingDog

---
+++ BrakingDog/src/com/company/BarkingDog.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class BarkingDog {
+
+    private String name;
+
+    public BarkingDog(String name) {
+        this.name = name;
+    }
+
+//    public boolean shouldWakeUp(boolean isBarking, int hourOfDay){
+//        if (hourOfDay > 8 && hourOfDay < 22){
+//            return false;
+//        } else if (isBarking && hourOfDay > 0 && hourOfDay < 24){
+//            return true;
+//        }
+//        return false;
+//    }
+
+    public boolean shouldWakeUp(boolean isBarking, int hourOfDay){
+        if (hourOfDay >= 8 && hourOfDay <= 22){
+            return false;
+        } else if (isBarking && hourOfDay > 0 && hourOfDay < 24){
+            return true;
+        }
+        return false;
+    }
+
+}
