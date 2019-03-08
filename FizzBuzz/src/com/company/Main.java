c56c08432db35c12202676cc5b8769ab6e99b22a
Jun Jian Shen <jshen9085@gmail.com>
2019-03-07 22:07:41 -0500

    fizzbuzz

---
+++ FizzBuzz/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    List result = FizzBuzz.fizzBuzz(30);
+        System.out.println(result);
+    }
+}
