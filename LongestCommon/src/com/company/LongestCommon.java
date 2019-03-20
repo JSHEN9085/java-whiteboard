18b505855411e8470c285d7c6956c2b9dabb889f
Jun Jian Shen <jshen9085@gmail.com>
2019-03-19 23:37:38 -0400

    horizontal scan

--- LongestCommon/src/com/company/LongestCommon.java
+++ LongestCommon/src/com/company/LongestCommon.java
@@ -21,8 +21,15 @@ public class LongestCommon {
 
 
         String prefix = strings[0];
-        for(int i = 0; i < strings.length; i++){
+        for(int i = 1; i < strings.length; i++){
+            while(strings[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length() - 1);
 
+                if(prefix.isEmpty()){
+                    return "";
+                }
+            }
         }
+        return prefix;
     }
 }
