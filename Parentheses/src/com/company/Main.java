c82acf0b26535544bd3a33a8ad28c2a085674f48
Jun Jian Shen <jshen9085@gmail.com>
2019-03-24 21:29:44 -0400

    valid Parentheses

--- Parentheses/src/com/company/Main.java
+++ Parentheses/src/com/company/Main.java
@@ -1,15 +1,22 @@
 package com.company;
 
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
-//	    String example = "abcdefg";
-//
-//        System.out.println(example.charAt(0)); //return 'a';
-//        System.out.println(example.toCharArray()); //return abcdefg with array format
-//        System.out.println(example.toCharArray()[1]); //return 'b';
+	    String example = "abcdefg";
 
+        System.out.println(example.charAt(0)); //return 'a';
+        System.out.println(example.toCharArray()); //return abcdefg with array format
+        System.out.println(example.toCharArray()[1]); //return 'b';
 
-        System.out.println(Parentheses.isValid("({"));
+        Stack<Character> stack = new Stack<>();
+        System.out.println(stack.push('a'));
+        System.out.println(stack.push('b')); //push will return the element you push in;
+        System.out.println("before stack.pop() " + stack); // return [a, b]
+        System.out.println(stack.pop()); // pop will return the element you pop out;
+        System.out.println("after stack.pop() " + stack); // return [a]
+
     }
 }
