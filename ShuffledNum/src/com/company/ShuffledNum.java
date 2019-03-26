25371e11a51af5a0e1e46b45eede8a151a4503de
Jun Jian Shen <jshen9085@gmail.com>
2019-03-25 23:07:16 -0400

    ShuffledNumber

--- ShuffledNum/src/com/company/ShuffledNum.java
+++ ShuffledNum/src/com/company/ShuffledNum.java
@@ -1,5 +1,8 @@
 package com.company;
 
+//given a int 123456, take the first digit first and then the last digit
+//finally return an int 162534
+
 public class ShuffledNum {
     public static int shuffledNum(int number){
         StringBuilder result = new StringBuilder();
@@ -12,7 +15,7 @@ public class ShuffledNum {
 
         if(charNumber.length % 2 != 0){
             result.setLength(result.length() - 1);
-        }
+        } //without this part 12345 will return 152433; !!!
 
         return Integer.parseInt(result.toString());
     }
