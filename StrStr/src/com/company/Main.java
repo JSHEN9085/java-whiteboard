20187cde8f931af1d08a5af8f08606c00a565c56
Jun Jian Shen <jshen9085@gmail.com>
2019-03-26 22:07:28 -0400

    StrStr

---
+++ StrStr/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    private static String string = "testing";
+
+    public static void main(String[] args) {
+        System.out.println(StrStr.strStr("abcde", "abcdef"));
+        System.out.println(string.indexOf('s'));
+    }
+}
