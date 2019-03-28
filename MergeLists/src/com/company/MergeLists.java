4bded6151fe51a4d2ff6e5dd5767aadf3dd71ce9
Jun Jian Shen <jshen9085@gmail.com>
2019-03-27 21:53:10 -0400

    merge lists

--- MergeLists/src/com/company/MergeLists.java
+++ MergeLists/src/com/company/MergeLists.java
@@ -1,4 +1,10 @@
 package com.company;
+//Merge two sorted linked lists and return it as a new list.
+//The new list should be made by splicing together the nodes of the first two lists.
+
+//Example:
+//Input: 1->2->4, 1->3->4
+//Output: 1->1->2->3->4->4
 
 public class MergeLists {
 
@@ -9,6 +15,29 @@ public class MergeLists {
     }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
+        //or write it as ListNode head = new ListNode(-1), temp = head;
 
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                temp.next = l1;
+                l1 = l1.next;
+            } else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+            temp.next = null;
+        }
+
+        if(l1 != null){
+            temp.next = l1;
+        }
+
+        if(l2 != null){
+            temp.next = l2;
+        }
+        return head.next;
     }
 }
