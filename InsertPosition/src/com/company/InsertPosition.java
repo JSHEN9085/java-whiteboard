4552f9c3cc865292e95281ca8e69025c8b320957
Jun Jian Shen <jshen9085@gmail.com>
2019-03-27 21:14:30 -0400

    insert position

--- InsertPosition/src/com/company/InsertPosition.java
+++ InsertPosition/src/com/company/InsertPosition.java
@@ -1,4 +1,17 @@
 package com.company;
+//Given a sorted array and a target value, return the index if
+//the target is found. If not, return the index where it
+//would be if it were inserted in order.
+//You may assume no duplicates in the array.
+
+//Input: [1,3,5,6], 5
+//Output: 2
+//
+//Input: [1,3,5,6], 7
+//Output: 4
+//
+//Input: [1,3,5,6], 0
+//Output: 0
 
 public class InsertPosition {
     public static int searchInsert(int[] nums, int target){
