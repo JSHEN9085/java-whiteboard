415f7658f59fdc010c33177b2ca4965ec61c60f5
Jun Jian Shen <jshen9085@gmail.com>
2019-04-18 23:45:50 -0400

    array to tree

---
+++ ArrayToTree/src/com/company/ArrayToTree.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class ArrayToTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; };
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums){
+        
+    }
+}
