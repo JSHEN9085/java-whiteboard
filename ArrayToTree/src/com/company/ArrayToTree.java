1e8e87ca52378c4f33dde5ce3f1daa37287cb477
Jun Jian Shen <jshen9085@gmail.com>
2019-04-19 22:04:54 -0400

    array to tree

--- ArrayToTree/src/com/company/ArrayToTree.java
+++ ArrayToTree/src/com/company/ArrayToTree.java
@@ -1,5 +1,15 @@
 package com.company;
 
+//Given the sorted array: [-10,-3,0,5,9],
+//One possible answer is: [0,-3,9,-10,null,5], which represents
+//the following height balanced BST:
+
+//      0
+//     / \
+//   -3   9
+//   /   /
+// -10  5
+
 public class ArrayToTree {
     public class TreeNode {
         int val;
@@ -9,6 +19,29 @@ public class ArrayToTree {
     }
 
     public TreeNode sortedArrayToBST(int[] nums){
-        
+        if(nums == null || nums.length == 0){
+            return null;
+        }
+
+        return getTreeNode(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode getTreeNode(int[] nums, int start, int end){
+        if(start > end){
+            return null;
+        }
+        int middle = (start + end) / 2;
+        TreeNode n = new TreeNode(nums[middle]);
+        n.left = getTreeNode(nums, start, middle - 1);
+        n.right = getTreeNode(nums, middle + 1, end);
+        return n;
     }
 }
+//hint: head of every subtree is always the middle point of the interval
+// [1, 2, 3, 4, 5, 6, 7]
+//         4
+//        / \
+//       2   6
+// like above example, 2 is the middle point between beginning and 4, so it must be the left node of 4
+// and then we keep finding the middle point and set it as left node, and we do
+// same thing at the right side. 
