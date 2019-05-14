ff836f25c41632694ea4b8b1c80a7752a661f667
JSHEN9085 <jshen9085@gmail.com>
2019-05-14 11:07:37 -0400

    first v

--- ValidPalindrome/src/com/jshen/Main.java
+++ ValidPalindrome/src/com/jshen/Main.java
@@ -3,6 +3,6 @@ package com.jshen;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(ValidPalindrome.isPalindrome("A man, a plan, a canal: Panama"));
     }
 }
--- ValidPalindrome/src/com/jshen/ValidPalindrome.java
+++ ValidPalindrome/src/com/jshen/ValidPalindrome.java
@@ -1,7 +1,20 @@
 package com.jshen;
 
 public class ValidPalindrome {
-    public boolean isPalindrome(String s) {
-        
+    public static boolean isPalindrome(String s) {
+        if(s == null || s.length() == 0){
+            return true;
+        }
+
+        String charOnly = s.replaceAll("[^a-zA-Z]+", "").toLowerCase();
+        System.out.println(charOnly);
+        for(int i = 0; i < charOnly.length() / 2; i++){
+            System.out.println(charOnly.charAt(i));
+            if(charOnly.charAt(i) != charOnly.charAt(charOnly.length() - 1 - i)){
+                return false;
+            }
+        }
+        return true;
     }
+
 }
