d21adbcd3f840b60aceb751c087cf210a5315de0
JSHEN9085 <jshen9085@gmail.com>
2020-03-27 22:41:52 -0400

    list cycle

--- ListCycle/src/com/jshen/ListCycleReview.java
+++ ListCycle/src/com/jshen/ListCycleReview.java
@@ -30,7 +30,21 @@ public class ListCycleReview {
 
     //version 2 without extra space;
     public boolean hasCycle(ListNode head) {
-        
+        ListNode slowPointer = head;
+        ListNode fastPointer = head; //we assign both slow and fast as head to avoid null exception. for list shorter than 2, we might get this error
+
+        while(fastPointer != null && fastPointer.next != null){
+            //these conditions are checking which position the fastpointer is located.
+            //if sit at second last at previous loop, fastpointer = fastpointer.next.next will bring fastpointer to be null
+            //if sit at third last at previous loop, fastpointer = fastpointer.next.next will bring fastpointer to be the last node, and inside the condition fastpointer.next will be null
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+
+            if(slowPointer == fastPointer){
+                return true;
+            }
+        }
+        return false;
     }
 
 }
