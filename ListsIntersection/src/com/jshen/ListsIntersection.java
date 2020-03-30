ef9bb201ad7e6c4f113c91e1fd870d999487a953
JSHEN9085 <jshen9085@gmail.com>
2020-03-29 22:04:06 -0400

    Lists Intersection

--- ListsIntersection/src/com/jshen/ListsIntersection.java
+++ ListsIntersection/src/com/jshen/ListsIntersection.java
@@ -32,8 +32,42 @@ public class ListsIntersection {
 //        return null;
 //    }
 
-//    version 2, faster one;
+//    version 2, faster one. two pointers method;
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        
+        ListNode pointerA = headA;
+        ListNode pointerB = headB;
+        int counterA = 0;
+        int counterB = 0;
+
+        while(pointerA != pointerB && counterA < 2 && counterB < 2){
+            if(pointerA == null){
+                pointerA = headB;
+                counterA++;
+            } else {
+                pointerA = pointerA.next;
+            }
+
+            if(pointerB == null){
+                pointerB = headA;
+                counterB++;
+            } else {
+                pointerB = pointerB.next;
+            }
+        }
+
+        if(pointerA == pointerB){
+            return pointerA;
+        } else {
+            return null;
+        }
     }
+//note for two pointer method
+//two lists have different length, key point, they have different length before the common portion(if they actually have intersection)
+//so, after pointerA went through listA, we restart it from the head of listB
+//identically, after pointerB went through listB, we restart it from the head of listA
+//so thing about the portion before the intersection, in term of their length, a + b = b + a
+//therefore, the key point is after the first iteration, both pointer will meet each other exactly at the intersection point.
+//another key thing
+//if those two lists do not have intersection, the above method will go forever
+//that's why I set two counters there, if both pointers travel both lists more than twice, and still not meet each other, that means lists don't have intersection
 }
