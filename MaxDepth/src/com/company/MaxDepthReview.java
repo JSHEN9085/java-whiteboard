1f54896e22c4c01372b476b89b67618d13b6d199
JSHEN9085 <jshen9085@gmail.com>
2020-04-02 23:28:41 -0400

    max depth bfs

--- MaxDepth/src/com/company/MaxDepthReview.java
+++ MaxDepth/src/com/company/MaxDepthReview.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MaxDepthReview {
     public class TreeNode {
         int val;
@@ -9,10 +13,35 @@ public class MaxDepthReview {
     }
 
 //  Solution 1 - Recursion
+//    public int maxDepth(TreeNode root) {
+//        if(root == null){
+//            return 0;
+//        }
+//        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+//    }
+
+//  solution 2 - BFS
     public int maxDepth(TreeNode root) {
-        if(root == null){
-            return 0;
+        if(root == null) return 0;
+
+        ArrayDeque<TreeNode> treeQueue = new ArrayDeque<>();
+        treeQueue.addLast(root);
+        int count = 0;
+        while(!treeQueue.isEmpty()){
+            int size = treeQueue.size();
+            while(size > 0){
+                TreeNode currentNode = treeQueue.poll();
+
+                if(currentNode.left != null){
+                    treeQueue.addLast(currentNode.left);
+                }
+                if(currentNode.right != null){
+                    treeQueue.addLast(currentNode.right);
+                }
+                size--;
+            }
+            count++;
         }
-        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        return count;
     }
 }
