84a2bc5ac3061a5717932905c15b813883fc0d03
JSHEN9085 <jshen9085@gmail.com>
2020-04-09 22:23:26 -0400

    1st missing positive

---
+++ ModeInBST/src/com/jshen/BST.java
@@ -0,0 +1,14 @@
+package com.jshen;
+
+public class BST {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public int[] findMode(TreeNode root) {
+
+    }
+}
--- PathSumIII/src/com/jshen/PathSum3.java
+++ PathSumIII/src/com/jshen/PathSum3.java
@@ -8,6 +8,11 @@ public class PathSum3 {
         TreeNode(int x) { val = x; }
     }
 
+    //solution with helper, this is brute force solution
+    //we need helper function because we are running two recursions here
+    //one is from root, and one is from its children
+    //its time is O(n^2), starts from root, fine any path, and then treat its children as root, and then run the whole process again
+    //that is why root goes into helper function and its children start from pathSum.
     public int pathSum(TreeNode root, int sum) {
         if(root == null) return 0;
         return getWays(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
@@ -21,19 +26,4 @@ public class PathSum3 {
         return ways;
     }
 
-
-//    public int pathSum(TreeNode root, int sum){
-//        int res = 0;
-//        if(root == null)
-//            return res;
-//        if(sum == root.val)
-//            res++;
-//        // without root
-//        res += pathSum(root.left, sum);
-//        res += pathSum(root.right, sum);
-//        // with root;
-//        res += pathSum(root.left, sum - root.val);
-//        res += pathSum(root.right, sum - root.val);
-//        return res;
-//    }
 }
