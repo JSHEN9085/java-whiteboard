fba8ad7c2e298316d1db9bd1994bf90e48f6ef16
JSHEN9085 <jshen9085@gmail.com>
2020-04-12 19:36:56 -0400

    Posrorder travel recursion

--- PostorderTravel/src/com/jshen/Postorder.java
+++ PostorderTravel/src/com/jshen/Postorder.java
@@ -15,26 +15,45 @@ public class Postorder {
     }
 
     //post order is left -> right -> root travel
+//    public List<Integer> postorderTraversal(TreeNode root) {
+//        List<Integer> result = new ArrayList<>();
+//        if(root == null) return result;
+//
+//        Stack<TreeNode> treeStack = new Stack<>();
+//        treeStack.push(root);
+//
+//        while(!treeStack.isEmpty()){
+//            TreeNode current = treeStack.pop();
+//            result.add(0, current.val); //key difference, we have [root] -> [right, root] -> [left -> right -> root] since we always add it to the first position
+//
+//            if(current.left != null){
+//                treeStack.push(current.left);
+//            }
+//            if(current.right != null){
+//                treeStack.push(current.right);
+//            }
+//        }
+//        return result;
+//    }
+
+    //recursion
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         if(root == null) return result;
-
-        Stack<TreeNode> treeStack = new Stack<>();
-        treeStack.push(root);
-
-        while(!treeStack.isEmpty()){
-            TreeNode current = treeStack.pop();
-            result.add(0, current.val); //key difference, we have [root] -> [right, root] -> [left -> right -> root] since we always add it to the first position
-
-            if(current.left != null){
-                treeStack.push(current.left);
-            }
-            if(current.right != null){
-                treeStack.push(current.right);
-            }
-        }
+        helper(root, result);
         return result;
     }
 
+    private void helper(TreeNode root, List<Integer> result){
+        if(root == null) return;
+        result.add(0, root.val);
+        helper(root.right, result);
+        helper(root.left, result);
+
+        //if just add to the list, use below order
+        //helper(root.left, result);
+        //helper(root.right, result);
+        //result.add(root.val);
+    }
 
 }
