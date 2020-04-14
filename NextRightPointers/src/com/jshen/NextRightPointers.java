c4118fec9f18006be5f9d1a3a2ad1bfd497c9564
JSHEN9085 <jshen9085@gmail.com>
2020-04-13 21:06:27 -0400

    Next right pointer O(n)

---
+++ NextRightPointers/src/com/jshen/NextRightPointers.java
@@ -0,0 +1,59 @@
+package com.jshen;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class NextRightPointers {
+    public class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    //iterate solution, time is O(n), space is O(n) 
+//    public Node connect(Node root) {
+//        if(root == null) return null;
+//
+//        Node head = root;
+//        LinkedList<Node> nodeQueue = new LinkedList<>();
+//        nodeQueue.add(root);
+//
+//        while(!nodeQueue.isEmpty()){
+//            int size = nodeQueue.size();
+//            while(size > 0){
+//                Node current = nodeQueue.poll();
+//                if(size > 1) {  //can't use if(nodeQueue.peek() != null) to determine, because queue is dynamic, while size is fixed for each level
+//                    current.next = nodeQueue.peek();
+//                } else {
+//                    current.next = null;
+//                }
+//
+//                if(current.left != null){
+//                    nodeQueue.add(current.left);
+//
+//                }
+//                if(current.right != null){
+//                    nodeQueue.add(current.right);
+//                }
+//                size--;
+//            }
+//        }
+//        return head;
+//    }
+}
